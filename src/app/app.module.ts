55e459a8e5589219536df8cbc12c83685960379c
Severin Spoerri <severin.spoerri@outlook.com>
2021-10-28 22:05:31 +0200

    add dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { StoreModule } from '@ngrx/store';
       BrowserAnimationsModule,
       TuiDialogModule,
       TuiNotificationsModule,
-      StoreModule.forRoot({}, {})
+      StoreModule.forRoot({}, {}),
+      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
 ],
   providers: [],
   bootstrap: [AppComponent]
