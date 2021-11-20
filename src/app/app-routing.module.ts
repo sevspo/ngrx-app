1d8432862535e547f06eb61ebc77ad93e6578483
Severin Spoerri <severin.spoerri@outlook.com>
2021-11-20 23:16:07 +0100

    added posts module and inst @ngrx/entity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
 ];
 
 @NgModule({
